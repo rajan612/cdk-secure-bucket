c9456f1b01e6a4f8549ec7ca1383854012101eea
rvashisht <rvashisht@vibrenthealth.com>
2025-07-14 16:08:43 -0400

    feat(init): bootstrap CDK project with TypeScript app

---
+++ evinova-coding-challenge/test/evinova-coding-challenge.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EvinovaCodingChallenge from '../lib/evinova-coding-challenge-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/evinova-coding-challenge-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EvinovaCodingChallenge.EvinovaCodingChallengeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
